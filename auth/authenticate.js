542e680c946dd0621d7d4acf45b9844f026c0fd1
mattr21 <matt.rothstein21@gmail.com>
2019-04-05 08:41:41 -0700

    added the authentication code - going to /api/jokes returns a list of jokes

--- auth/authenticate.js
+++ auth/authenticate.js
@@ -1,8 +1,9 @@
 const jwt = require('jsonwebtoken');
+const secrets = require('../config/secrets.js');
 
-const jwtKey =
-  process.env.JWT_SECRET ||
-  'add a .env file to root of project with the JWT_SECRET variable';
+// const jwtKey =
+//   process.env.JWT_SECRET ||
+//   'add a .env file to root of project with the JWT_SECRET variable';
 
 // quickly see what this file exports
 module.exports = {
@@ -14,7 +15,7 @@ function authenticate(req, res, next) {
   const token = req.get('Authorization');
 
   if (token) {
-    jwt.verify(token, jwtKey, (err, decoded) => {
+    jwt.verify(token, secrets.jwtSecret, (err, decoded) => {
       if (err) return res.status(401).json(err);
 
       req.decoded = decoded;
