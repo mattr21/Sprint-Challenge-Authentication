5e30c05bddbf33491887ff427821b01f5ef6c5fd
mattr21 <matt.rothstein21@gmail.com>
2019-04-05 08:23:31 -0700

    implemented register function that uses bcrypt to hash the password

--- config/routes.js
+++ config/routes.js
@@ -1,4 +1,6 @@
 const axios = require('axios');
+const Users = require('./models.js');
+const bcrypt = require('bcryptjs');
 
 const { authenticate } = require('../auth/authenticate');
 
@@ -10,6 +12,17 @@ module.exports = server => {
 
 function register(req, res) {
   // implement user registration
+  let user = req.body;
+  const hash = bcrypt.hashSync(user.password, 4);
+  user.password = hash;
+
+  Users.add(user)
+    .then(saved => {
+      res.status(201).json(saved);
+    })
+    .catch(error => {
+      res.status(500).json(error);
+    })
 }
 
 function login(req, res) {
