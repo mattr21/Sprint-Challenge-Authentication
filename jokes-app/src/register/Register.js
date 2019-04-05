d997af273d12366c2b859f5e9ac8d6b9fb7ff94e
mattr21 <matt.rothstein21@gmail.com>
2019-04-05 09:21:42 -0700

    added Home and Register components

---
+++ jokes-app/src/register/Register.js
@@ -0,0 +1,67 @@
+import React from 'react';
+import axios from 'axios';
+
+class Register extends React.Component {
+    state = {
+        username: "",
+        password: "",
+    };
+
+    render () {
+        return (
+            <>
+                <form onSubmit={this.handleSubmit}>
+                    <div>
+                        <label htmlFor="username" />
+                        <input 
+                            value={this.state.username}
+                            onChange={this.handleInputChange}
+                            id="username"
+                            name="username"
+                            type="text"
+                            placeholder="username"
+                        />
+                    </div>
+                    <div>
+                        <label htmlFor="password" />
+                        <input 
+                            value={this.state.password}
+                            onChange={this.handleInputChange}
+                            id="password"
+                            name="password"
+                            type="password"
+                            placeholder="password"
+                        />
+                    </div>
+                    <div>
+                        <button type="submit">Register</button>
+                    </div>
+                </form>
+            </>
+        );
+    }
+
+    handleSubmit = event => {
+        event.preventDefault();
+
+        const endpoint = "http://localhost:3300/api/register";
+        axios
+            .post(endpoint, this.state)
+            .then(res => {
+                // console.log("LOGIN RESPONSE:", res);
+                localStorage.setItem('token', res.data.token);
+                this.props.history.push('/login');
+            })
+            .catch(error => {
+                console.log("REGISTER ERROR:", error);
+            })
+    }
+
+    handleInputChange = event => {
+        const { name, value } = event.target;
+        this.setState({ [name]: value });
+    }
+    
+}
+
+export default Register;
